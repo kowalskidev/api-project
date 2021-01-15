23706d76878863d4147cb1dd034552079ec520bc
kowalskidev <zkesarani@gmail.com>
2021-01-15 15:09:38 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// parse request bodies (req.body)
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use(bodyParser.json());
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.params);
   res.render("index");
 });
 
